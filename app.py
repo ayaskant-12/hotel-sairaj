4c17e55db954f2ded54fd4240d9e66e83ecccff3
Ayaskant Dash <104549230+ayaskant-12@users.noreply.github.com>
2025-06-27 04:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1314,7 +1314,4 @@ def create_admin_user():
         # Handle error as before...
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        create_admin_user()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
